3d76a1b1ed9a9fb1f17a186a6753bc99fa1c5679
George Ruiz <georuiz817@gmail.com>
2019-05-15 21:07:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,8 @@ end
 
 
 def not_safe?(speed)
-  speed > 60 ? true : false 
-  speed < 40 ? true : false 
+  speed > 60 ? true : true 
+  speed < 40 ? true : true  
   speed > 60 and speed < 40 ? true: false 
 end 
 
